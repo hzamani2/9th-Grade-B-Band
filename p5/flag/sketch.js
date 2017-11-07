7bb97cd52ef2ed9aa949310b784a6679273e6543
hzamani2 <hmojtabaee-zamani@packer.edu>
2017-11-07 10:59:38 -0500

    commented code

--- p5/flag/sketch.js
+++ p5/flag/sketch.js
@@ -1,9 +1,21 @@
 function setup() {
+
+	//create canvas
 	createCanvas (windowWidth, windowHeight);
+
+	//remove black outline around shapes
 	noStroke();
+
+	//green fill
 	fill (0, 255, 0);
+
+	//green section of Italian flag
 	rect (0, 0, width/3, height);
+
+	//red fill
 	fill (255, 0, 0);
+
+	//red section of Italian flag
 	rect (2 * width/3, 0, width/3, height);
 
 }
